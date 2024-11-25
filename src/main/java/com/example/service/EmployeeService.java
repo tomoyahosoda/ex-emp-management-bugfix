2ab7c25f8b05f0ae56e1801b5a60d917129035a9
tomoya.hosoda <tomoya.hosoda@rakus-partners.co.jp>
2024-11-25 16:56:47 +0900

    serviceにメソッド追加

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -52,4 +52,9 @@ public class EmployeeService {
 	public void update(Employee employee) {
 		employeeRepository.update(employee);
 	}
+
+	public List<Employee> findByName(String name) {
+		List<Employee> employeeList = employeeRepository.findByName(name);
+		return employeeList;
+	}
 }
